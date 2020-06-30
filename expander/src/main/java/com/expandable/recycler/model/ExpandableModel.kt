777c6ce234e6cfb44b5813a32b9354c20f027b1b
Ibrahim Ali <eng.ibrahim222@gmail.com>
2020-06-30 15:28:00 +0200

    pub version 1.0.1

--- expander/src/main/java/com/expandable/recycler/model/ExpandableModel.kt
+++ expander/src/main/java/com/expandable/recycler/model/ExpandableModel.kt
@@ -1,7 +1,10 @@
 package com.expandable.recycler.model
 
+import com.expandable.recycler.PARENT
+
 data class ExpandableModel(
-    val id: Int,
-    val title: String,
-    val type: Int
+    val id: Int = 0,
+    val title: String = "",
+    val type: Int = PARENT,
+    val url: String = ""
 )
